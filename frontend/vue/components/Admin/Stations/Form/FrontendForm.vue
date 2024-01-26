a13ef0800703465accff722da93b795ddeba34d7
Sven <scysys@outlook.com>
2024-01-26 11:56:06 +0100

    0.9.5.1

--- frontend/vue/components/Admin/Stations/Form/FrontendForm.vue
+++ frontend/vue/components/Admin/Stations/Form/FrontendForm.vue
@@ -231,7 +231,7 @@
 import BFormFieldset from "~/components/Form/BFormFieldset.vue";
 import BWrappedFormGroup from "~/components/Form/BWrappedFormGroup.vue";
 import {FRONTEND_ICECAST, FRONTEND_REMOTE, FRONTEND_SHOUTCAST} from "~/components/Entity/RadioAdapters";
-import objectToFormOptions from "~/functions/objectToFormOptions";
+/*import objectToFormOptions from "~/functions/objectToFormOptions";*/
 import {computed} from "vue";
 import {useTranslate} from "~/vendor/gettext";
 
